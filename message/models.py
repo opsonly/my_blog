b46adc85aceff742d73dc586073fcc0c5a5565f6
opsonly <13271331235@163.com>
2018-12-06 15:34:45 +0800

    '完善留言系统'

--- message/models.py
+++ message/models.py
@@ -19,7 +19,7 @@ class Commit(models.Model):
     name = models.CharField(max_length=20,verbose_name='回复人姓名',null=False)
     text = models.TextField(verbose_name='回复内容',null=False,blank=True,default="")
     create_time = models.DateTimeField(default=timezone.now,verbose_name='创建时间',null=False,blank=False)
-
+    mid = models.IntegerField(default=0)
     class Meta:
         verbose_name = '回复内容'
         ordering = ['-create_time']
